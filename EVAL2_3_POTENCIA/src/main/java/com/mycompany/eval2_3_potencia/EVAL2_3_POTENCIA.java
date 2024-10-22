27d4cc86c70501d1bf604f6de9d40102bc724bc3
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-10-22 11:52:26 -0600

    Add files via upload

---
+++ EVAL2_3_POTENCIA/src/main/java/com/mycompany/eval2_3_potencia/EVAL2_3_POTENCIA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval2_3_potencia;
+
+/**
+ *
+ * @author everv
+ */
+public class EVAL2_3_POTENCIA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
