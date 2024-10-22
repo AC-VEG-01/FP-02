933d2f0a0c63c1201e05c2af92e3c499c4f4b785
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-10-22 11:40:29 -0600

    Add files via upload

---
+++ EVAL2_2_CALIFICACION/src/main/java/com/mycompany/eval2_2_calificacion/EVAL2_2_CALIFICACION.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval2_2_calificacion;
+
+/**
+ *
+ * @author everv
+ */
+public class EVAL2_2_CALIFICACION {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
