619d2636a3d3797bd68b6a835479e03512c066a2
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-10-28 23:10:42 -0600

    Add files via upload

---
+++ EVAL2_1__CICLOS/src/main/java/com/mycompany/eval2_1__ciclos/EVAL2_1__CICLOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval2_1__ciclos;
+
+
+/**
+ *
+ * @author everv
+ */
+public class EVAL2_1__CICLOS {
+
+    public static void main(String[] args) {
+         for(int i = 1; i <= 10; i++){
+            System.out.print(i +" - ");
+        }
+        for(int x = 10; x <= 1; x++){
+            System.out.print(x +" - ");
+        }
+        
+    }
+    
+}
+    
